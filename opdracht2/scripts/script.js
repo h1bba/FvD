748d4fd642f006d97affe876c5b153d991875ede
Ufuk <hibbayt@gmail.com>
2023-04-11 13:38:40 +0200

    Added bingo card

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,6 +1,4 @@
 // JavaScript Document
-console.log("howdy");
-
 // Declare an empty array to store the generated random numbers
 var randomNumbers = [];
 var randomNumbersList = document.querySelector("#randomNumbersList");
@@ -24,4 +22,47 @@ pullNumberBtn.addEventListener("click", function () {
   var liPulled = document.createElement("li"); // create a new list item element
   liPulled.textContent = randomNumber; // set its text content to the generated number
   randomNumbersList.insertBefore(liPulled, randomNumbersList.firstChild); // insert the list item at the beginning of the unordered list
+
+  // Check if the generated number matches with any number in the bingo card list
+  var bingoCardList = document.querySelectorAll("#bingoCardList li");
+  var matchedCount = 0;
+  for (var i = 0; i < bingoCardList.length; i++) {
+    if (bingoCardList[i].textContent == randomNumber) {
+      bingoCardList[i].classList.add("matched");
+      matchedCount++;
+    }
+  }
+
+  // Check if all the numbers in the bingo card have been matched
+  if (matchedCount == bingoCardList.length) {
+    alert("Congratulations! You have matched all the numbers on your bingo card.");
+  }
 });
+
+// Declare an empty array to store the bingo card numbers
+var bingoCardNumbers = [];
+
+// Generate 20 unique random numbers between 0 and 30
+while (bingoCardNumbers.length < 20) {
+  var randomNumber = Math.floor(Math.random() * 31); // generate a random number between 0 and 30
+  if (!bingoCardNumbers.includes(randomNumber)) { // check if the random number is not already in the array
+    bingoCardNumbers.push(randomNumber); // add the random number to the array
+  }
+}
+
+// Display the bingo card numbers on the screen
+var bingoCardList = document.querySelector("#bingoCardList");
+for (var i = 0; i < bingoCardNumbers.length; i++) {
+  var li = document.createElement("li");
+  li.textContent = bingoCardNumbers[i];
+  bingoCardList.appendChild(li);
+
+  // Add a click event listener to each li element in the bingo card list
+  li.addEventListener("click", function () {
+    // Check if the clicked li element matches with any number in the randomNumbers array
+    var clickedNumber = parseInt(this.textContent);
+    if (randomNumbers.includes(clickedNumber)) {
+      this.classList.add("matched");
+    }
+  });
+}
